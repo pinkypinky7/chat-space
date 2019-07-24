987dd40db5f4d131e4cc0be40cba63072e3c3ad7
kominaminaoki <pinkypinky0331@gmail.com>
2019-07-24 13:36:01 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -87,16 +87,13 @@ $(document).on('turbolinks:load', function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-    
         //追加するHTMLの入れ物を作る
         var insertHTML = '';
-        
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる   
         messages.forEach( function(message){
-         
+          
+          message.image? var img = `<img src=${message.image} ></img>` : var img =`<img src= "" ></img>`
             
-            message.image? var img = `<img src=${message.image} ></img>` : var img =`<img src= "" ></img>`
-
             var html =
              `<div class="message" data-id=${message.id}>
                 <div class="upper-message">
